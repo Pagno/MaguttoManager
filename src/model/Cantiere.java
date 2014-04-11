5054348ecd83e00122091ed67306dda83009dc88
Valmoz <valmoz_gtt@yahoo.it>
2014-04-11 17:35:30 +0200

    Add Cantiere class

---
+++ src/model/Cantiere.java
@@ -0,0 +1,12 @@
+package model;
+import java.util.*;
+public class Cantiere {
+	String nomeCantiere;
+	String indirizzo;
+	Date dataApertura;
+	Date dataChiusura;
+	
+	public Cantiere(){
+		//TODO
+	}
+}
