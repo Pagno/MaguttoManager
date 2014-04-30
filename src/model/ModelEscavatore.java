ac762a135759d3fd530a84997894249015570544
Matteo <pagnoncelli.teo@gmail.com>
2014-04-30 11:17:03 +0200

    Escavatore

---
+++ src/model/ModelEscavatore.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+class ModelEscavatore {
+	private ArrayList<Escavatore> listaEscavatori;
+	
+	public ModelEscavatore(){
+		listaEscavatori=new ArrayList<Escavatore>();
+	}
+
+	public void modificaEscavatore(int codice, String produttore, String Modello,int capacita,int portata,int altezza,int profondita) {
+		Iterator<Escavatore> itr = listaEscavatori.iterator();
+	    while (itr.hasNext()) {
+	      if(itr.next().getCodice()==codice){;
+	    	  itr.next().setProduttore(produttore);
+	    	  itr.next().setModello(Modello);
+	    	  itr.next().setCapacitaMassima(capacita);
+	    	  itr.next().setPortataMassima( portata);
+	    	  itr.next().setProfonditaMassima(profondita);
+	    	  itr.next().setAltezzaMassima(altezza);
+	      }
+	    }
+	}
+	
+	public boolean eliminaEscavatore(int codice){
+		Iterator<Escavatore> itr = listaEscavatori.iterator();
+	    while (itr.hasNext()) {
+	    	if(itr.next().getCodice()==codice){
+		    	 itr.remove();
+		    	 return true;
+	    	}
+	    }
+		return false;		
+	}
+}
