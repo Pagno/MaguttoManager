275402db60d3444e2655d35923bcc39e211f8e98
Matteo <pagnoncelli.teo@gmail.com>
2014-05-15 15:58:52 +0200

    modifica Gru

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -4,12 +4,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
 import javax.swing.JOptionPane;
 import view.InserimentoGru;
 import view.InserimentoRuspa;
 import view.MainView;
-
 import model.ModelConnector;
 
 public class MainController{
