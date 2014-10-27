c50e9a12fed179111722106ddd014ca068fb456d
Pagno <pagnoncelli.teo@gmail.com>
2014-10-27 13:31:48 +0100

    JavaDoc Lavoro,RichiestaCamion,RichiestaEscavatore e RichiestaGru

--- src/model/organizer/data/RichiestaEscavatore.java
+++ src/model/organizer/data/RichiestaEscavatore.java
@@ -1,15 +1,47 @@
 package model.organizer.data;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class RichiestaEscavatore.
+ */
 public class RichiestaEscavatore extends RichiestaMacchina {
+	
+	/** The min capacita. */
 	int minCapacita;
+	
+	/** The max capacita. */
 	int maxCapacita;
+	
+	/** The min portata. */
 	int minPortata;
+	
+	/** The max portata. */
 	int maxPortata;
+	
+	/** The min altezza. */
 	int minAltezza;
+	
+	/** The max altezza. */
 	int maxAltezza;
+	
+	/** The min profondita. */
 	int minProfondita;
+	
+	/** The max profondita. */
 	int maxProfondita;
 	
+	/**
+	 *Crea una nuova richiesta per gli escavatori.
+	 *
+	 * @param minCapacita the min capacita
+	 * @param maxCapacita the max capacita
+	 * @param minPortata the min portata
+	 * @param maxPortata the max portata
+	 * @param minAltezza the min altezza
+	 * @param maxAltezza the max altezza
+	 * @param minProfondita the min profondita
+	 * @param maxProfondita the max profondita
+	 */
 	public RichiestaEscavatore(int minCapacita, int maxCapacita,
 			int minPortata, int maxPortata, int minAltezza, int maxAltezza,
 			int minProfondita, int maxProfondita) {
@@ -24,70 +56,153 @@ public class RichiestaEscavatore extends RichiestaMacchina {
 		this.maxProfondita = maxProfondita;
 	}
 
+	/**
+	 * Ritorna minima capacita.
+	 *
+	 * @return minima capacita
+	 */
 	public int getMinCapacita() {
 		return minCapacita;
 	}
 
+	/**
+	 * Assegna minima capacita.
+	 *
+	 * @param minCapacita minima capacita
+	 */
 	public void setMinCapacita(int minCapacita) {
 		this.minCapacita = minCapacita;
 	}
 
+	/**
+	 * Ritorna massima capacita.
+	 *
+	 * @return massima capacita
+	 */
 	public int getMaxCapacita() {
 		return maxCapacita;
 	}
 
+	/**
+	 * Assegna massima capacita.
+	 *
+	 * @param maxCapacita massima capacita
+	 */
 	public void setMaxCapacita(int maxCapacita) {
 		this.maxCapacita = maxCapacita;
 	}
 
+	/**
+	 * Ritorna minima portata.
+	 *
+	 * @return minima portata
+	 */
 	public int getMinPortata() {
 		return minPortata;
 	}
 
+	/**
+	 * Assegna minima portata.
+	 *
+	 * @param minPortata minima portata
+	 */
 	public void setMinPortata(int minPortata) {
 		this.minPortata = minPortata;
 	}
 
+	/**
+	 * Ritorna massima portata.
+	 *
+	 * @return massima portata
+	 */
 	public int getMaxPortata() {
 		return maxPortata;
 	}
 
+	/**
+	 * Assegna massima portata.
+	 *
+	 * @param maxPortata massima portata
+	 */
 	public void setMaxPortata(int maxPortata) {
 		this.maxPortata = maxPortata;
 	}
 
+	/**
+	 * Ritorna minima altezza.
+	 *
+	 * @return minima altezza
+	 */
 	public int getMinAltezza() {
 		return minAltezza;
 	}
 
+	/**
+	 * Assegna minima altezza.
+	 *
+	 * @param minPortata minima altezza
+	 */
 	public void setMinAltezza(int minAltezza) {
 		this.minAltezza = minAltezza;
 	}
 
+	/**
+	 * Ritorna massima altezza.
+	 *
+	 * @return massima altezza
+	 */
 	public int getMaxAltezza() {
 		return maxAltezza;
 	}
 
+	/**
+	 * Assegna massima altezza.
+	 *
+	 * @param maxPortata massima altezza
+	 */
 	public void setMaxAltezza(int maxAltezza) {
 		this.maxAltezza = maxAltezza;
 	}
 
+	/**
+	 * Ritorna minima profondita.
+	 *
+	 * @return minima profondita
+	 */
 	public int getMinProfondita() {
 		return minProfondita;
 	}
 
+	/**
+	 * Assegna minima profondita.
+	 *
+	 * @param minPortata minima profondita
+	 */
 	public void setMinProfondita(int minProfondita) {
 		this.minProfondita = minProfondita;
 	}
 
+	/**
+	 * Ritorna massima profondita.
+	 *
+	 * @return massima profondita
+	 */
 	public int getMaxProfondita() {
 		return maxProfondita;
 	}
 
+	/**
+	 * Assegna massima profondita.
+	 *
+	 * @param maxPortata massima profondita
+	 */
 	public void setMaxProfondita(int maxProfondita) {
 		this.maxProfondita = maxProfondita;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return  "Richiesta:Escavatore " + minCapacita
@@ -100,6 +215,9 @@ public class RichiestaEscavatore extends RichiestaMacchina {
 
 
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -128,6 +246,9 @@ public class RichiestaEscavatore extends RichiestaMacchina {
 		return true;
 	}
 
+	/* (non-Javadoc)
+	 * @see model.organizer.data.RichiestaMacchina#rispettaRichiesta(model.organizer.data.Macchina)
+	 */
 	@Override
 	public boolean rispettaRichiesta(Macchina m) {
 		if (m == null)
--- src/model/organizer/data/RichiestaGru.java
+++ src/model/organizer/data/RichiestaGru.java
@@ -1,15 +1,47 @@
 package model.organizer.data;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class RichiestaGru.
+ */
 public class RichiestaGru extends RichiestaMacchina {
+	
+	/** The min lunghezza. */
 	private int minLunghezza;
+	
+	/** The max lunghezza. */
 	private int maxLunghezza;
+	
+	/** The min altezza. */
 	private int minAltezza;
+	
+	/** The max altezza. */
 	private int maxAltezza;
+	
+	/** The min portata. */
 	private int minPortata;
+	
+	/** The max portata. */
 	private int maxPortata;
+	
+	/** The min angolo rotazione. */
 	private int minAngoloRotazione;
+	
+	/** The max angolo rotazione. */
 	private int maxAngoloRotazione;
 	
+	/**
+	 * Crea una nuova richiesta per le Gru.
+	 *
+	 * @param minLunghezza the min lunghezza
+	 * @param maxLunghezza the max lunghezza
+	 * @param minAltezza the min altezza
+	 * @param maxAltezza the max altezza
+	 * @param minPortata the min portata
+	 * @param maxPortata the max portata
+	 * @param minAngoloRotazione the min angolo rotazione
+	 * @param maxAngoloRotazione the max angolo rotazione
+	 */
 	public RichiestaGru(int minLunghezza, int maxLunghezza, int minAltezza,
 			int maxAltezza, int minPortata, int maxPortata,
 			int minAngoloRotazione, int maxAngoloRotazione) {
@@ -24,70 +56,153 @@ public class RichiestaGru extends RichiestaMacchina {
 		this.maxAngoloRotazione = maxAngoloRotazione;
 	}
 
+	/**
+	 * Ritorna minima lunghezza.
+	 *
+	 * @return minima lunghezza
+	 */
 	public int getMinLunghezza() {
 		return minLunghezza;
 	}
 
+	/**
+	 * Assegna minima lunghezza.
+	 *
+	 * @param minLunghezza minima lunghezza
+	 */
 	public void setMinLunghezza(int minLunghezza) {
 		this.minLunghezza = minLunghezza;
 	}
 
+	/**
+	 * Ritorna massima lunghezza.
+	 *
+	 * @return massima lunghezza
+	 */
 	public int getMaxLunghezza() {
 		return maxLunghezza;
 	}
 
+	/**
+	 * Assegna massima lunghezza.
+	 *
+	 * @param maxLunghezza massima lunghezza
+	 */
 	public void setMaxLunghezza(int maxLunghezza) {
 		this.maxLunghezza = maxLunghezza;
 	}
 
+	/**
+	 * Ritorna minima altezza.
+	 *
+	 * @return minima altezza
+	 */
 	public int getMinAltezza() {
 		return minAltezza;
 	}
 
+	/**
+	 * Assegna minima altezza.
+	 *
+	 * @param minPortata minima altezza
+	 */
 	public void setMinAltezza(int minAltezza) {
 		this.minAltezza = minAltezza;
 	}
 
+	/**
+	 * Ritorna massima altezza.
+	 *
+	 * @return massima altezza
+	 */
 	public int getMaxAltezza() {
 		return maxAltezza;
 	}
 
+	/**
+	 * Assegna massima altezza.
+	 *
+	 * @param maxPortata massima altezza
+	 */
 	public void setMaxAltezza(int maxAltezza) {
 		this.maxAltezza = maxAltezza;
 	}
 
+	/**
+	 * Ritorna minima portata.
+	 *
+	 * @return minima portata
+	 */
 	public int getMinPortata() {
 		return minPortata;
 	}
 
+	/**
+	 * Assegna minima portata.
+	 *
+	 * @param minPortata minima portata
+	 */
 	public void setMinPortata(int minPortata) {
 		this.minPortata = minPortata;
 	}
 
+	/**
+	 * Ritorna massima portata.
+	 *
+	 * @return massima portata
+	 */
 	public int getMaxPortata() {
 		return maxPortata;
 	}
 
+	/**
+	 * Assegna massima portata.
+	 *
+	 * @param maxPortata massima portata
+	 */
 	public void setMaxPortata(int maxPortata) {
 		this.maxPortata = maxPortata;
 	}
 
+	/**
+	 * Ritorna minimo angolo di rotazione.
+	 *
+	 * @return minimo angolo di rotazione
+	 */
 	public int getMinAngoloRotazione() {
 		return minAngoloRotazione;
 	}
 
+	/**
+	 * Assegna minimo angolo di rotazione.
+	 *
+	 * @param minAngoloRotazione minimo angolo di rotazione
+	 */
 	public void setMinAngoloRotazione(int minAngoloRotazione) {
 		this.minAngoloRotazione = minAngoloRotazione;
 	}
 
+	/**
+	 * Ritorna massimo angolo di rotazione.
+	 *
+	 * @return massimo angolo di rotazione
+	 */
 	public int getMaxAngoloRotazione() {
 		return maxAngoloRotazione;
 	}
 
+	/**
+	 * Assegna massimo angolo di rotazione.
+	 *
+	 * @param maxAngoloRotazione massimo angolo di rotazione
+	 */
 	public void setMaxAngoloRotazione(int maxAngoloRotazione) {
 		this.maxAngoloRotazione = maxAngoloRotazione;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -116,6 +231,9 @@ public class RichiestaGru extends RichiestaMacchina {
 		return true;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return "Richiesta:Gru " + minLunghezza + "-"
@@ -125,6 +243,9 @@ public class RichiestaGru extends RichiestaMacchina {
 				+ "-" + maxAngoloRotazione ;
 	}
 
+	/* (non-Javadoc)
+	 * @see model.organizer.data.RichiestaMacchina#rispettaRichiesta(model.organizer.data.Macchina)
+	 */
 	@Override
 	public boolean rispettaRichiesta(Macchina m) {
 		if (m == null)
