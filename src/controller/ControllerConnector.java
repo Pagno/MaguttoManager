5e2e09fc6962eb87e877c485fc566deb07c8002f
Valmoz <valmoz_gtt@yahoo.it>
2014-10-27 14:27:21 +0100

    Javadoc - Controller connector e Interface

--- src/controller/ControllerConnector.java
+++ src/controller/ControllerConnector.java
@@ -16,17 +16,29 @@ import controller.data.Associazione;
 import controller.organizer.*;
 
 
+
+/**
+ * The Class ControllerConnector.
+ */
 public class ControllerConnector implements ControllerInterface{
+	
+	/** The app ctrl. */
 	private ControllerApplicazione appCtrl;
+	
+	/** The ins ctrl. */
 	private ControllerInserimento insCtrl;
+	
+	/** The can ctrl. */
 	private ControllerCantiere canCtrl;
 	
+	/** The istanza. */
 	private static ControllerConnector istanza;
 
 
 	/**
 	 * Gets   model gru.
 	 *
+	 * @param modelInterface the model interface
 	 * @return   model gru
 	 */
 	public static synchronized ControllerConnector getControllerConnector(ModelInterface modelInterface){
@@ -36,6 +48,11 @@ public class ControllerConnector implements ControllerInterface{
 		return istanza;
 	}
 	
+	/**
+	 * Instantiates a new controller connector.
+	 *
+	 * @param modelConnector the model connector
+	 */
 	private ControllerConnector(ModelInterface modelConnector) {
 		appCtrl=ControllerApplicazione.getControllerApplicazione(modelConnector);
 		insCtrl=ControllerInserimento.getControllerInserimento(modelConnector);
@@ -43,58 +60,101 @@ public class ControllerConnector implements ControllerInterface{
 	}
 	
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#aggiungiGruObserver(java.util.Observer)
+	 */
 	public void aggiungiGruObserver(Observer observer){
 		appCtrl.aggiungiGruObserver(observer);
 	}
+	
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#aggiungiRuspaObserver(java.util.Observer)
+	 */
 	public void aggiungiRuspaObserver(Observer observer){
 		appCtrl.aggiungiRuspaObserver(observer);
 	}
+	
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#aggiungiCamionObserver(java.util.Observer)
+	 */
 	public void aggiungiCamionObserver(Observer observer){
 		appCtrl.aggiungiCamionObserver(observer);
 	}	
+	
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#aggiungiEscavatoreObserver(java.util.Observer)
+	 */
 	public void aggiungiEscavatoreObserver(Observer observer){
 		appCtrl.aggiungiEscavatoreObserver(observer);
 	}
+	
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#aggiungiCantiereObserver(java.util.Observer)
+	 */
 	public void aggiungiCantiereObserver(Observer observer){
 		appCtrl.aggiungiCantiereObserver(observer);
 	}
 	
 	
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#eliminaMacchina(java.lang.Integer)
+	 */
 	@Override
 	public boolean eliminaMacchina(Integer codiceMacchina) {
 		return appCtrl.eliminaMacchina(codiceMacchina);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#eliminaCantiere(java.lang.Integer)
+	 */
 	@Override
 	public boolean eliminaCantiere(Integer codiceCantiere) {
 		return appCtrl.eliminaCantiere(codiceCantiere);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#exitManager()
+	 */
 	@Override
 	public void exitManager() {
 		appCtrl.exitManager();
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#caricaDatiListener()
+	 */
 	@Override
 	public void caricaDatiListener() {
 		appCtrl.caricaDatiListener();
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#salvaDatiListener()
+	 */
 	@Override
 	public void salvaDatiListener() {
 		appCtrl.salvaDatiListener();
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#chiusuraProgramma()
+	 */
 	@Override
 	public void chiusuraProgramma() {
 		appCtrl.chiusuraProgramma();
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#getCantiere(int)
+	 */
 	@Override
 	public Cantiere getCantiere(int codiceCantiere) {
 		return canCtrl.getCantiere(codiceCantiere);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#modificaCantiere(int, java.lang.String, java.lang.String, java.util.GregorianCalendar, java.util.GregorianCalendar, model.organizer.data.Priorita)
+	 */
 	@Override
 	public boolean modificaCantiere(int codiceCantiere, String nomeCantiere,
 			String indirizzo, GregorianCalendar dataApertura,
@@ -103,33 +163,50 @@ public class ControllerConnector implements ControllerInterface{
 		return canCtrl.modificaCantiere(codiceCantiere, nomeCantiere, indirizzo, dataApertura, dataChiusura, priorita);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#aggiungiLavoro(int, java.lang.String, java.util.GregorianCalendar, java.util.GregorianCalendar)
+	 */
 	@Override
 	public boolean aggiungiLavoro(int codiceCantiere, String nomeLavoro,
 			GregorianCalendar dataInizio, GregorianCalendar dataFine) {
-		// TODO Auto-generated method stub
 		return canCtrl.aggiungiLavoro(codiceCantiere, nomeLavoro, dataInizio, dataFine);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#eliminaLavoro(int)
+	 */
 	@Override
 	public boolean eliminaLavoro(int codiceLavoro) {
 		return canCtrl.eliminaLavoro(codiceLavoro);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#eliminaRichiesta(int)
+	 */
 	@Override
 	public boolean eliminaRichiesta(int codiceRichiesta) {
 		return canCtrl.eliminaRichiesta(codiceRichiesta);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#getElencoMacchineDisponibili(int)
+	 */
 	@Override
 	public ArrayList<Macchina> getElencoMacchineDisponibili(int codiceRichiesta) {
 		return canCtrl.getElencoMacchineDisponibili(codiceRichiesta);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#liberaRichiesta(int)
+	 */
 	@Override
 	public boolean liberaRichiesta(int codiceRichiesta) {
 		return canCtrl.liberaRichiesta(codiceRichiesta);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#aggiungiRichiesta(int, int, model.organizer.data.RichiestaMacchina)
+	 */
 	@Override
 	public boolean aggiungiRichiesta(int codiceCantiere, int codiceLavoro,
 			RichiestaMacchina richiesta) {
@@ -137,21 +214,33 @@ public class ControllerConnector implements ControllerInterface{
 		return canCtrl.aggiungiRichiesta(codiceCantiere, codiceLavoro, richiesta);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#soddisfaRichiesta(int, int)
+	 */
 	@Override
 	public boolean soddisfaRichiesta(int codiceRichiesta, int codiceMacchina) {
 		return canCtrl.soddisfaRichiesta(codiceRichiesta, codiceMacchina);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#generaAssociazioni()
+	 */
 	@Override
 	public ArrayList<Associazione> generaAssociazioni() {
 		return canCtrl.generaAssociazioni();
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#confermaAssociazioniListener(java.util.ArrayList)
+	 */
 	@Override
 	public void confermaAssociazioniListener(ArrayList<Associazione> data) {
 		canCtrl.confermaAssociazioniListener(data);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#modificaLavoro(int, java.lang.String, java.util.GregorianCalendar, java.util.GregorianCalendar)
+	 */
 	@Override
 	public void modificaLavoro(int codiceLavoro, String nome,
 			GregorianCalendar inizio, GregorianCalendar fine) {
@@ -159,6 +248,9 @@ public class ControllerConnector implements ControllerInterface{
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#aggiungiGru(java.lang.String, java.lang.String, int, int, int, int)
+	 */
 	@Override
 	public boolean aggiungiGru(String produttore, String modello,
 			int rotazione, int portata, int lunghezza, int altezza) {
@@ -166,6 +258,9 @@ public class ControllerConnector implements ControllerInterface{
 		return insCtrl.aggiungiGru(produttore, modello, rotazione, portata, lunghezza, altezza);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#aggiungiRuspa(java.lang.String, java.lang.String, int, int, int)
+	 */
 	@Override
 	public boolean aggiungiRuspa(String produttore, String modello,
 			int capacita, int portata, int altezza) {
@@ -173,12 +268,18 @@ public class ControllerConnector implements ControllerInterface{
 		return insCtrl.aggiungiRuspa(produttore, modello, capacita, portata, altezza);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#aggiungiEscavatore(java.lang.String, java.lang.String, int, int, int, int)
+	 */
 	@Override
 	public boolean aggiungiEscavatore(String produttore, String modello,
 			int capacita, int portata, int altezza, int profondita) {
 		return insCtrl.aggiungiEscavatore(produttore, modello, capacita, portata, altezza, profondita);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#aggiungiCamion(java.lang.String, java.lang.String, int, int, int)
+	 */
 	@Override
 	public boolean aggiungiCamion(String produttore, String modello,
 			int capacita, int portata, int lunghezza) {
@@ -186,6 +287,9 @@ public class ControllerConnector implements ControllerInterface{
 		return insCtrl.aggiungiCamion(produttore, modello, capacita, portata, lunghezza);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#aggiungiCantiere(java.lang.String, java.lang.String, java.util.GregorianCalendar, java.util.GregorianCalendar, model.organizer.data.Priorita)
+	 */
 	@Override
 	public boolean aggiungiCantiere(String nomeCantiere,
 			String indirizzo, GregorianCalendar dataApertura,
@@ -194,12 +298,18 @@ public class ControllerConnector implements ControllerInterface{
 		return insCtrl.aggiungiCantiere(nomeCantiere, indirizzo, dataApertura, dataChiusura, priorita);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#modificaCamion(int, java.lang.String, java.lang.String, int, int, int)
+	 */
 	@Override
 	public boolean modificaCamion(int codiceCamion, String produttore,
 			String modello, int capacita, int portata, int lunghezza) {
 		return insCtrl.modificaCamion(codiceCamion, produttore, modello, capacita, portata, lunghezza);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#modificaGru(int, java.lang.String, java.lang.String, int, int, int, int)
+	 */
 	@Override
 	public boolean modificaGru(int codiceGru, String produttore,
 			String modello, int rotazione, int portata, int lunghezza,
@@ -208,6 +318,9 @@ public class ControllerConnector implements ControllerInterface{
 		return insCtrl.modificaGru(codiceGru, produttore, modello, rotazione, portata, lunghezza, altezza);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#modificaRuspa(int, java.lang.String, java.lang.String, int, int, int)
+	 */
 	@Override
 	public boolean modificaRuspa(int codiceRuspa, String produttore,
 			String modello, int capacita, int portata, int altezza) {
@@ -215,6 +328,9 @@ public class ControllerConnector implements ControllerInterface{
 		return insCtrl.modificaRuspa(codiceRuspa, produttore, modello, capacita, portata, altezza);
 	}
 
+	/* (non-Javadoc)
+	 * @see controller.ControllerInterface#modificaEscavatore(int, java.lang.String, java.lang.String, int, int, int, int)
+	 */
 	@Override
 	public boolean modificaEscavatore(int codiceEscavatore, String produttore,
 			String modello, int capacita, int portata, int altezza,
--- src/controller/ControllerInterface.java
+++ src/controller/ControllerInterface.java
@@ -10,53 +10,311 @@ import model.organizer.data.Priorita;
 import model.organizer.data.RichiestaMacchina;
 import controller.data.Associazione;
 
+
+/**
+ * The Interface ControllerInterface.
+ */
 public interface ControllerInterface {
 	
 	//Application Interface
+	/**
+	 * Elimina macchina.
+	 *
+	 * @param codiceMacchina codice della macchina
+	 * @return true, if successful
+	 */
 	public boolean eliminaMacchina(Integer codiceMacchina);	
+	
+	/**
+	 * Elimina cantiere.
+	 *
+	 * @param codiceCantiere codice del cantiere
+	 * @return true, if successful
+	 */
 	public boolean eliminaCantiere(Integer codiceCantiere);	
+	
+	/**
+	 * Exit manager.
+	 */
 	public void exitManager();
+	
+	/**
+	 * Carica dati listener.
+	 */
 	public void caricaDatiListener();
+	
+	/**
+	 * Salva dati listener.
+	 */
 	public void salvaDatiListener();
+	
+	/**
+	 * Chiusura programma.
+	 */
 	public void chiusuraProgramma();
 	//
+	/**
+	 * Aggiungi gru observer.
+	 *
+	 * @param observer the observer
+	 */
 	public void aggiungiGruObserver(Observer observer);
+	
+	/**
+	 * Aggiungi ruspa observer.
+	 *
+	 * @param observer the observer
+	 */
 	public void aggiungiRuspaObserver(Observer observer);
+	
+	/**
+	 * Aggiungi camion observer.
+	 *
+	 * @param observer the observer
+	 */
 	public void aggiungiCamionObserver(Observer observer);	
+	
+	/**
+	 * Aggiungi escavatore observer.
+	 *
+	 * @param observer the observer
+	 */
 	public void aggiungiEscavatoreObserver(Observer observer);	
+	
+	/**
+	 * Aggiungi cantiere observer.
+	 *
+	 * @param observer the observer
+	 */
 	public void aggiungiCantiereObserver(Observer observer);
 	
 	//CantieriInterface
+	/**
+	 * Gets the cantiere.
+	 *
+	 * @param codiceCantiere codice del antiere
+	 * @return the cantiere
+	 */
 	public  Cantiere getCantiere(int codiceCantiere);
+	
+	/**
+	 * Modifica cantiere.
+	 *
+	 * @param codiceCantiere codice del cantiere
+	 * @param nomeCantiere nome del cantiere
+	 * @param indirizzo indirizzo del cantiere
+	 * @param dataApertura data apertura
+	 * @param dataChiusura data chiusura
+	 * @param priorita priorita del cantiere
+	 * @return true, if successful
+	 */
 	public  boolean modificaCantiere(int codiceCantiere,String nomeCantiere,String indirizzo,GregorianCalendar dataApertura,GregorianCalendar dataChiusura,Priorita priorita );
+	
+	/**
+	 * Aggiungi lavoro.
+	 *
+	 * @param codiceCantiere codice del cantiere
+	 * @param nomeLavoro nome del lavoro
+	 * @param dataInizio data inizio
+	 * @param dataFine data fine
+	 * @return true, if successful
+	 */
 	public  boolean aggiungiLavoro(int codiceCantiere,String nomeLavoro,GregorianCalendar dataInizio,GregorianCalendar dataFine);
 
 
-	//public  void aggiungiRichiestaObserver(Observer observer );
 	
+	/**
+	 * Elimina lavoro.
+	 *
+	 * @param codiceLavoro codice del lavoro
+	 * @return true, if successful
+	 */
 	public  boolean eliminaLavoro(int codiceLavoro);
+	
+	/**
+	 * Elimina richiesta.
+	 *
+	 * @param codiceRichiesta codice della richiesta
+	 * @return true, if successful
+	 */
 	public  boolean eliminaRichiesta(int codiceRichiesta);
+	
+	/**
+	 * Gets the elenco macchine disponibili.
+	 *
+	 * @param codiceRichiesta codice della richiesta
+	 * @return elenco delle macchine disponibili
+	 */
 	public   ArrayList<Macchina>  getElencoMacchineDisponibili(int codiceRichiesta);
 	
+	/**
+	 * Libera richiesta.
+	 *
+	 * @param codiceRichiesta codice della richiesta
+	 * @return true, if successful
+	 */
 	public  boolean liberaRichiesta(int codiceRichiesta);
+	
+	/**
+	 * Aggiungi richiesta.
+	 *
+	 * @param codiceCantiere codice del cantiere
+	 * @param codiceLavoro codice del lavoro
+	 * @param richiesta richiesta
+	 * @return true, if successful
+	 */
 	public  boolean aggiungiRichiesta(int codiceCantiere,int codiceLavoro,RichiestaMacchina richiesta);
+	
+	/**
+	 * Soddisfa richiesta.
+	 *
+	 * @param codiceRichiesta codice della richiesta
+	 * @param codiceMacchina codice della macchina
+	 * @return true, if successful
+	 */
 	public  boolean soddisfaRichiesta(int codiceRichiesta, int codiceMacchina) ;
+	
+	/**
+	 * Genera associazioni.
+	 *
+	 * @return array list
+	 */
 	public  ArrayList<Associazione> generaAssociazioni();
+	
+	/**
+	 * Conferma associazioni listener.
+	 *
+	 * @param data the data
+	 */
 	public  void confermaAssociazioniListener(ArrayList<Associazione> data);
+	
+	/**
+	 * Modifica lavoro.
+	 *
+	 * @param codiceLavoro the codice lavoro
+	 * @param nome the nome
+	 * @param inizio the inizio
+	 * @param fine the fine
+	 */
 	public  void modificaLavoro(int codiceLavoro, String nome,GregorianCalendar inizio, GregorianCalendar fine);
 
 	//Insert Interface
+	/**
+	 * Aggiungi gru.
+	 *
+	 * @param produttore produttore
+	 * @param modello modello
+	 * @param rotazione rotazione
+	 * @param portata portata
+	 * @param lunghezza lunghezza
+	 * @param altezza altezza
+	 * @return true, if successful
+	 */
 	public  boolean aggiungiGru(String produttore,String modello,int rotazione, int portata,int lunghezza,int altezza);
+	
+	/**
+	 * Aggiungi ruspa.
+	 *
+	 * @param produttore produttore
+	 * @param modello modello
+	 * @param capacita capacita
+	 * @param portata portata
+	 * @param altezza altezza
+	 * @return true, if successful
+	 */
 	public  boolean aggiungiRuspa(String produttore, String modello,int capacita,int portata,int altezza);
+	
+	/**
+	 * Aggiungi escavatore.
+	 *
+	 * @param produttore produttore
+	 * @param Modello modello
+	 * @param capacita capacita
+	 * @param portata portata
+	 * @param altezza altezza
+	 * @param profondita profondita
+	 * @return true, if successful
+	 */
 	public  boolean aggiungiEscavatore(String produttore, String Modello,int capacita,int portata,int altezza,int profondita);
+	
+	/**
+	 * Aggiungi camion.
+	 *
+	 * @param produttore produttore
+	 * @param Modello modello
+	 * @param capacita capacita
+	 * @param portata portata
+	 * @param lunghezza lunghezza
+	 * @return true, if successful
+	 */
 	public  boolean aggiungiCamion(String produttore,String Modello,int capacita,int portata,int lunghezza);
+	
+	/**
+	 * Aggiungi cantiere.
+	 *
+	 * @param nomeCantiere nome cantiere
+	 * @param indirizzo indirizzo
+	 * @param dataApertura data apertura
+	 * @param dataChiusura data chiusura
+	 * @param priorita priorita
+	 * @return true, if successful
+	 */
 	public  boolean aggiungiCantiere(String nomeCantiere,String indirizzo,GregorianCalendar dataApertura,GregorianCalendar dataChiusura,Priorita priorita );
 	
 	
 	
+	/**
+	 * Modifica camion.
+	 *
+	 * @param codiceCamion codice camion
+	 * @param produttore produttore
+	 * @param modello modello
+	 * @param capacita capacita
+	 * @param portata portata
+	 * @param lunghezza lunghezza
+	 * @return true, if successful
+	 */
 	public  boolean modificaCamion(int codiceCamion,String produttore,String modello,int capacita,int portata,int lunghezza);
+	
+	/**
+	 * Modifica gru.
+	 *
+	 * @param codiceGru codice gru
+	 * @param produttore produttore
+	 * @param modello modello
+	 * @param rotazione rotazione
+	 * @param portata portata
+	 * @param lunghezza lunghezza
+	 * @param altezza altezza
+	 * @return true, if successful
+	 */
 	public  boolean modificaGru(int codiceGru,String produttore,String modello,int rotazione, int portata,int lunghezza,int altezza) ;
+	
+	/**
+	 * Modifica ruspa.
+	 *
+	 * @param codiceRuspa codice ruspa
+	 * @param produttore produttore
+	 * @param modello modello
+	 * @param capacita capacita
+	 * @param portata portata
+	 * @param altezza altezza
+	 * @return true, if successful
+	 */
 	public  boolean modificaRuspa(int codiceRuspa,String produttore, String modello,int capacita,int portata,int altezza) ;
+	
+	/**
+	 * Modifica escavatore.
+	 *
+	 * @param codiceEscavatore codice escavatore
+	 * @param produttore produttore
+	 * @param modello modello
+	 * @param capacita capacita
+	 * @param portata portata
+	 * @param altezza altezza
+	 * @param profondita profondita
+	 * @return true, if successful
+	 */
 	public  boolean modificaEscavatore(int codiceEscavatore,String produttore, String modello,int capacita,int portata,int altezza,int profondita);
 		
 }
